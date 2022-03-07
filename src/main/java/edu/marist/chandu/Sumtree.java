fc0fc3cd2a0752528f70256b1250569e1f67b831
chandurao77 <98127784+chandurao77@users.noreply.github.com>
2022-03-07 10:33:07 -0500

    binary tree

---
+++ src/main/java/edu/marist/chandu/Sumtree.java
@@ -0,0 +1,29 @@
+package com.chandu.app.binary_tree;
+
+public class SumTree {
+
+    private Node root;
+
+    public SumTree(Node root) {
+        this.root = root;
+    }
+
+    public boolean isSumTree() {
+        if (this.root == null) return false;
+        if (this.root.getRight() == null && this.root.getLeft() == null) return false;
+
+        int sum = getSum(this.root.getLeft()) + getSum(this.root.getRight());
+        if (sum == this.root.getValue())
+            return true;
+        else
+            return false;
+
+    }
+
+    private int getSum(Node node) {
+        if (node == null)
+            return 0;
+        else
+            return getSum(node.getLeft()) + (Integer)node.getValue() + getSum(node.getRight());
+    }
+}
